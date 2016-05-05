66f8b16cfbd4fd9d9146ca42c25f53a141027818
Tanner Grehawick <ggrehawi@gmu.edu>
2016-05-05 13:57:42 -0400

    can now switch gravity modes in editor

--- Assets/script/GravityReciever.cs
+++ Assets/script/GravityReciever.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class GravityReciever : MonoBehaviour {
 
+	public Mode gravityMode = Mode.Greatest;
+
+	public enum Mode { Greatest, Total }
+
 	public Rigidbody body { get; private set; }
 
 	void Awake() {
@@ -10,26 +14,28 @@ public class GravityReciever : MonoBehaviour {
 
 	public Vector3 GetGravity() { return GetGravity(body.position); }
 	public Vector3 GetGravity(Vector3 position) {
-		Vector3 gMax = Vector3.zero;
+		Vector3 g = Vector3.zero;
 		foreach (GravitySource source in GravitySource.all) {
-			Vector3 g = source.GetGravity(position);
-			if (g.sqrMagnitude > gMax.sqrMagnitude) {
-				gMax = g;
-			}
+			Vector3 sourceG = source.GetGravity(position);
+			if (gravityMode == Mode.Total)
+				g += sourceG;
+			if (gravityMode == Mode.Greatest && sourceG.sqrMagnitude > g.sqrMagnitude)
+				g = sourceG;
 		}
-		return gMax;
+		return g;
 	}
 
 	public float GetGravityPotential() { return GetGravityPotential(body.position); }
 	public float GetGravityPotential(Vector3 position) {
-		float uGMax = 0;
+		float uG = 0;
 		foreach (GravitySource source in GravitySource.all) {
-			float uG = source.GetGravityPotential(position);
-			if (uG < uGMax) {
-				uGMax = uG;
-			}
+			float sourceUG = source.GetGravityPotential(position);
+			if (gravityMode == Mode.Total)
+				uG += sourceUG;
+			if (gravityMode == Mode.Greatest && sourceUG < uG)
+				uG = sourceUG;
 		}
-		return uGMax;
+		return uG;
 	}
 
 	void FixedUpdate() {
